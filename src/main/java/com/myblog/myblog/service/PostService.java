83b86e0da7c3a083028c286ad515ecea28f9ac56
Administrator <sahuabhay3100@gmail.com>
2024-01-23 23:12:57 +0530

    127:class added on 4th jan 2024

--- src/main/java/com/myblog/myblog/service/PostService.java
+++ src/main/java/com/myblog/myblog/service/PostService.java
@@ -2,7 +2,13 @@ package com.myblog.myblog.service;
 
 import com.myblog.myblog.payload.PostDto;
 
+import java.util.List;
+
 public interface PostService {
 
     PostDto createPost(PostDto postDto);
+
+    PostDto getPostById(Long id);
+
+    List<PostDto> getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
 }
